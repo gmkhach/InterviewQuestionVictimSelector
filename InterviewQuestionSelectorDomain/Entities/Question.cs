4caa01e3241bb5dc73e98adcfe10e3851988520a
Gabriel Khachatryan <gmkhach@gmail.com>
2018-11-07 22:55:38 -0500

    Entity Question is created

---
+++ InterviewQuestionSelectorDomain/Entities/Question.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewQuestionSelectorDomain.Entities
+{
+    class Question
+    {
+        public Question(string text, int questionID)
+        {
+            Text = text;
+            QuestionID = questionID;
+        }
+
+        public string Text { get; set; }
+        public int QuestionID { get; set; }
+    }
+}
