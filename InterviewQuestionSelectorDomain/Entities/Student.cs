16a261f9e31b007aacd05932b85d36073ab09cc4
Gabriel Khachatryan <gmkhach@gmail.com>
2018-11-07 22:53:27 -0500

    Entity Student is created

---
+++ InterviewQuestionSelectorDomain/Entities/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewQuestionSelectorDomain.Entities
+{
+    class Student
+    {
+        public Student(string firstName, string lastName, int studentID)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            StudentID = studentID;
+        }
+
+        public string FirstName { get; set; }   
+        public string LastName { get; set; }
+        public int StudentID { get; set; }
+    }
+}
